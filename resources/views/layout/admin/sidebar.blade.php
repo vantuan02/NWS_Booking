e1e11526ccfabf2e3a662e132f252b9344c89d96
vantuan02 <vantuan334456@gmail.com>
2024-11-26 15:17:29 +0700

    import lai vi loi phien ban laravel

--- resources/views/layout/admin/sidebar.blade.php
+++ resources/views/layout/admin/sidebar.blade.php
@@ -3,10 +3,10 @@
     <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
             <li class="nav-item">
-                <a href="" class="nav-link">
+                <a href="{{route('hotels.index')}}" class="nav-link">
                     <i class="nav-icon fa-regular fa-image"></i>
                     <p>
-                        Banner
+                        Hotel Manage
                     </p>
                 </a>
             </li>
